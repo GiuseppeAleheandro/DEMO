100e2c12b637cec77d9e89c9761b059ee0c3bf9b
GiuseppeAleheandro <128283213+GiuseppeAleheandro@users.noreply.github.com>
2023-03-22 18:50:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
+import numpy as np
+import cv2
 import subprocess
 import os
 import importlib
 
 import time
 os.system("clear")
-print("Checking All Requirements ...")
+print("Downloading All Requirements ...\n\n")
+os.system("pip3 install numpy")
+os.system("pip3 install flask")
+
 time.sleep(3)
-with open('requirements.txt', 'r') as f:
-    modules = f.read()
-# Try to import the module
-try:
-    os.system('clear')
-    importlib.import_module(modules)
-    import cv2
-    import numpy as np
-    from flask import Flask, render_template, Response
-    camera = cv2.VideoCapture(0)
 
-    def gen_frames():
-        while True:
-            success, frame = camera.read()
-            if not success:
-                break
-            else:
-                # Convert the frame to a byte array
-                ret, buffer = cv2.imencode('.jpg', frame)
-                frame_bytes = buffer.tobytes()
+os.system('clear')
+from flask import Flask, render_template, Response
+camera = cv2.VideoCapture(0)
 
-                # Yield the byte array as an HTTP response
-                yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            # Convert the frame to a byte array
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame_bytes = buffer.tobytes()
+
+            # Yield the byte array as an HTTP response
+            yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
     @app.route('/')
     def index():
@@ -41,11 +40,5 @@ try:
 
     if __name__ == '__main__':
         app.run(debug=True)
-
-    
-except ImportError:
-    os.system('clear')
-    print("Err: Please Run pip3 install -r requirements.txt")
-    exit()
 
 app = Flask(__name__)
